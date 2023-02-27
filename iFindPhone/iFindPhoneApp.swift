1a370ce812d66c22edf8a0df98fe4430d1039976
Minsu Han <minsu.medison@gmail.com>
2023-02-27 14:10:17 +0900

    Initial Commit

---
+++ iFindPhone/iFindPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFindPhoneApp.swift
+//  iFindPhone
+//
+//  Created by Soda on 2023/02/27.
+//
+
+import SwiftUI
+
+@main
+struct iFindPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
